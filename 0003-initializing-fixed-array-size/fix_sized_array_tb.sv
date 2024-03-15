f85780349c902a4971f44d7996b5d0975a064767
saba-ja <saba.janamian.663@my.csun.edu>
2024-03-15 14:25:32 -0700

    added new syntax for defining array

--- 0003-initializing-fixed-array-size/fix_sized_array_tb.sv
+++ 0003-initializing-fixed-array-size/fix_sized_array_tb.sv
@@ -4,8 +4,18 @@ module fix_sized_array_tb();
         int arr_x[4];
         int arr_y[5];
 
-        bit [0:4][31:0] src;
-        bit [0:4][31:0] dst;
+        localparam int ARR_SIZE = 5;
+
+        // The following will define an array of 5 elements with each element 32 bit 
+        // The index of each item is ascending (0 to 4) and the index of bits is decending (31 downto 0)
+        bit [0:ARR_SIZE-1][31:0] src0;
+        bit [0:ARR_SIZE-1][31:0] dst0;
+
+        // The following generates the same 2D array as the above with a different syntax.
+        // In the syntax the order of each element is declared first then the number of elements in the array
+        bit [31:0] src1[ARR_SIZE];
+        bit [31:0] dst1[ARR_SIZE];
+
         // ---------------------------------------------------------------------
         // Initializing arrays and priting elements using foreach
         // ---------------------------------------------------------------------
@@ -20,18 +30,31 @@ module fix_sized_array_tb();
         // ---------------------------------------------------------------------
         // Initializing arrays and priting elements using for loop
         // ---------------------------------------------------------------------
-        $display("Original Values:");
-        for (int i=0; i < $size(src); i++) begin
-            src[i] = i;
-            $display("src[%0d] = %0d", i, src[i]);
+        $display("src0 original Values:");
+        for (int i=0; i < $size(src0); i++) begin
+            src0[i] = i;
+            $display("src0[%0d] = %0d", i, src0[i]);
         end
         
-        $display("New Values:");
-        foreach (dst[i]) begin
-            dst[i] = src[i] * 2;
-            $display("dst[%0d] = %0d", i, dst[i]);
+        $display("dst0 new Values:");
+        foreach (dst0[i]) begin
+            dst0[i] = src0[i] * 2;
+            $display("dst0[%0d] = %0d", i, dst0[i]);
         end
-        
+
+        $display("src1 original Values:");
+        for (int i=0; i < $size(src1); i++) begin
+            src1[i] = i + 1;
+            $display("src1[%0d] = %0d", i, src1[i]);
+        end
+
+        $display("dst1 new Values:");
+        foreach (dst1[i]) begin
+            dst1[i] = src1[i] * 2;
+            $display("dst1[%0d] = %0d", i, dst1[i]);
+        end
+
+
         // These methoeds are not support in the simulator Icarus, 
         // but are supported in Mentor QuestaSim
         
---
+++ 0004-printing-multi-dimentional-array/print_multi_dim_array_tb.sv
@@ -0,0 +1,6 @@
+module print_multi_dim_array_tb();
+
+    initial begin
+        byte array_2d[4][6]
+    end
+endmodule
